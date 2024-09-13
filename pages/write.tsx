e744c532ca8690db8ce35aa64ece7fd56d5f0e00
yongchane <202021113@sangmyung.kr>
2024-09-13 18:02:39 +0900

    글쓰기 페이지 내용 작성 뷰 완성

--- pages/write.tsx
+++ pages/write.tsx
@@ -3,10 +3,13 @@ import styled from "styled-components";
 export default function Write() {
   return (
     <Container>
-      <InputContainer>
+      <InputTitleContainer>
         <StyledInput placeholder="제목" />
         <Divider />
-      </InputContainer>
+      </InputTitleContainer>
+      <InputContentContainer>
+        <StyledInputContent placeholder="내용을 입력하세요" />
+      </InputContentContainer>
     </Container>
   );
 }
@@ -16,7 +19,12 @@ const Container = styled.div`
   width: 100%;
 `;
 
-const InputContainer = styled.div`
+const InputTitleContainer = styled.div`
+  margin-top: 5px;
+  margin-left: 10px;
+  margin-right: 10px;
+`;
+const InputContentContainer = styled.div`
   margin-top: 5px;
   margin-left: 10px;
   margin-right: 10px;
@@ -27,9 +35,21 @@ const StyledInput = styled.input`
   width: 100%;
   height: 30px;
   font-weight: 600;
+  padding: 10px;
   ::placeholder {
     color: #d4d6d9; /* placeholder 색상 지정 */
   }
+`;
+const StyledInputContent = styled.textarea`
+  border: none;
+  width: 100%;
+  height: 1000px;
+  font-weight: 600;
+  resize: none; /* 사용자가 크기 조정 못하게 */
+  ::placeholder {
+    color: #d4d6d9; /* placeholder 색상 지정 */
+  }
+  padding: 10px; /* 텍스트가 너무 모서리에 붙지 않도록 패딩 추가 */
 `;
 
 const Divider = styled.div`
