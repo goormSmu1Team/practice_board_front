5924e1dee631671983806ca84df14d0f4c0aaebb
yongchane <202021113@sangmyung.kr>
2024-09-14 16:21:16 +0900

    댓글 뷰 연동

--- next.config.js
+++ next.config.js
@@ -1,19 +1,19 @@
-// next.config.js
-
 module.exports = {
   async rewrites() {
     return [
       {
-        source: "/api/proxy/:id", // 클라이언트에서 호출하는 경로
-        destination: "http://121.185.8.167:9090/boards/:id", // 실제 API 서버 경로
+        source: "/api/proxy/:id",
+        destination: "http://121.185.8.167:9090/boards/:id",
       },
+      // {
+      //   source: "/api/proxy/:id/comments",
+      //   destination: "http://121.185.8.167:9090/boards/:id/comments",
+      // },
     ];
   },
 
-  // React Strict Mode 활성화
   reactStrictMode: true,
 
-  // 웹팩 설정 (SVG 파일을 처리하기 위한 설정)
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
